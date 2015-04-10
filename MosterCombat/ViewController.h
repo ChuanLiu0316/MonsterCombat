0c59b7116f18e0a9b7a784b1289a25132cc12a4e
QINGWEI LAN <qingweilan@gmail.com>
2015-04-09 20:48:52 -0700

    Initial Commit

---
+++ MosterCombat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MosterCombat
+//
+//  Created by QINGWEI on 4/9/15.
+//  Copyright (c) 2015 qingweilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
